e8ae6f1199a3b7c2d97ab037636b5ff94f6ffe91
bloodyraven <bloody.duj@gmail.com>
2016-09-14 17:31:05 +0200

    retournage de carte + shuffle

--- src/ui/PlateauPanel.java
+++ src/ui/PlateauPanel.java
@@ -32,34 +32,46 @@ public class PlateauPanel extends JPanel {
          ArrayList<Carte> listCartes = game.getListCartes();
          
          //horizontal haut
-         if(listCartes.get(0).isDiscovered()) {
-        	 g.drawImage(listCartes.get(0).getImageTurned(),Constante.X6,Constante.Y1,null);
-         } else {
-        	 g.drawImage(rotate90DX(Constante.c0),Constante.X6,Constante.Y1,null);
-         }
-         g.drawImage(rotate90DX(Constante.c0),Constante.X8,Constante.Y1,null);
+         drawCarte(g, Constante.X6, Constante.Y1, listCartes.get(0), true);
+         drawCarte(g, Constante.X8, Constante.Y1, listCartes.get(1), true);
          
          //horizontal mid
-         g.drawImage(rotate90DX(Constante.c0),Constante.X3,Constante.Y5,null);
-         g.drawImage(rotate90DX(Constante.c0),Constante.X5,Constante.Y5,null);
-         g.drawImage(rotate90DX(Constante.c0),Constante.X7,Constante.Y5,null);
+         drawCarte(g, Constante.X3, Constante.Y5, listCartes.get(2), true);
+         drawCarte(g, Constante.X5, Constante.Y5, listCartes.get(3), true);
+         drawCarte(g, Constante.X7, Constante.Y5, listCartes.get(4), true);
          
          //horizontal bas
-         g.drawImage(rotate90DX(Constante.c0),Constante.X2,Constante.Y8,null);
-         g.drawImage(rotate90DX(Constante.c0),Constante.X4,Constante.Y8,null);
+         drawCarte(g, Constante.X2, Constante.Y8, listCartes.get(5), true);
+         drawCarte(g, Constante.X4, Constante.Y8, listCartes.get(6), true);
          
          //vertical gauche
-         g.drawImage(Constante.c0,Constante.X1,Constante.Y9,null);
-         g.drawImage(Constante.c0,Constante.X1,Constante.Y4,null);
+         drawCarte(g, Constante.X1, Constante.Y9, listCartes.get(7), false);
+         drawCarte(g, Constante.X1, Constante.Y4, listCartes.get(8), false);
          
          //vertical mid
-         g.drawImage(Constante.c0,Constante.X6,Constante.Y3,null);
-         g.drawImage(Constante.c0,Constante.X6,Constante.Y6,null);
+         drawCarte(g, Constante.X6, Constante.Y3, listCartes.get(9), false);
+         drawCarte(g, Constante.X6, Constante.Y6, listCartes.get(10), false);
          
          //vertical droite
-         g.drawImage(Constante.c0,Constante.X9,Constante.Y5,null);
-         g.drawImage(Constante.c0,Constante.X9,Constante.Y7,null);
+         drawCarte(g, Constante.X9, Constante.Y5, listCartes.get(11), false);
+         drawCarte(g, Constante.X9, Constante.Y7, listCartes.get(12), false);
      }
+	 
+	 private void drawCarte(Graphics g, int x, int y, Carte c, boolean turned) {
+		 if(c.isDiscovered()) {
+			 if(turned) {
+				 g.drawImage(c.getImageTurned(),x,y,null);
+			 } else {
+				 g.drawImage(c.getImage(),x,y,null);
+			 }
+         } else {
+        	 if(turned) {
+        		 g.drawImage(rotate90DX(Constante.c0),x,y,null);
+        	 } else {
+        		 g.drawImage(Constante.c0,x,y,null);
+        	 }
+         }
+	 }
 	 
 	 private BufferedImage rotate90DX(BufferedImage bi) {
 	    int width = bi.getWidth();
